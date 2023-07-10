120fb6630086e1f92176a973945bf5b883aa43f9
KyleM021199 <kylem021199@gmail.com>
2023-07-10 15:22:40 -0500

    completed some work on userController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,5 +2,16 @@ const { ObjectId } = require('mongoose').Types;
 const {User, Thought} = require('../models');
 
 module.exports = {
+    //for /thoughts route
+    //Get all thoughts
+    
+    //Get single thought by _id
+
+    // Post a new thought
+
+
+    // Update a thought
+
+    // Delete a thought 
 
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,75 @@
 const { ObjectId } = require('mongoose').Types;
 const {User, Thought} = require('../models');
-
+//friendCount instance method
+//to count the number of tied to the user
+const friendCount = async () => {
+    const numberOfFriends = await User.aggregate().count('friendCount');
+    return numberOfFriends;
+}
 module.exports ={
 
+
+
+//for /users route
+// Get all users
+async getUsers(req, res){
+try{
+const users = await User.find()
+.populate({path: 'friends', select: '-__v'},{path: 'thoughts', select: '-__v'});
+
+const userObj = {
+    users,
+    friendCount: await friendCount(), 
+}
+res.json(userObj);
+}catch (err){
+    console.log(err);
+    return res.status(500).json(err);
+}
+},
+// Get single user by id_
+//populate friend and thought data
+async getSingleUser(req, res){
+    try{
+    const user = await User.findOne({_id: req.params.userId})
+    .populate({path: 'friends', select: '-__v'},{path: 'thoughts', select: '-__v'});
+        if (!user){
+            return res.status(404).json({ message: 'No user with that ID'});
+        }
+        res.json(user);
+    }catch (err){
+        console.log(err);
+        return res.status(500).json(err);
+    }
+},
+
+//Post a new user
+async createUser(req, res) {
+    try{
+
+    }catch (err){
+        console.log(err);
+        return res.status(500).json(err);
+    }
+},
+
+// Update a user by _id
+async updateUser(req, res) {
+    try{
+
+    }catch (err){
+        console.log(err);
+        return res.status(500).json(err);
+    }
+},
+
+//Delete a user by _id
+async deleteUser(req, res) {
+    try{
+
+    }catch (err){
+        console.log(err);
+        return res.status(500).json(err);
+    }
+},
 } 
